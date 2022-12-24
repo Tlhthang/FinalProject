4011798e99aac1f9ad5b1729187de802a958198a
Tlhthang <95119466+Tlhthang@users.noreply.github.com>
2022-12-24 10:16:15 +0700

    Delete Web App directory

--- Web App/Run.py
+++
@@ -1,7 +0,0 @@
-import sys
-import streamlit as st
-from streamlit.web import cli as stcli
-
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "KOL Ranking.py"]
-    sys.exit(stcli.main())
